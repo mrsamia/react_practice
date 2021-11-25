aea2bfc80c873f87e08ef5cb176a9c7be2bf1ff1
mrsamia <15203060@iubat.edu>
2021-11-25 22:41:30 +0600

    SRM| add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App(){
   return(
     
     <div>
+      <h1>Our Medical Services </h1>
+      <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ducimus veniam illo quibusdam </h4>
       <Button></Button>
     </div>
    
