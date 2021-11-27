9362d009a7b5dbad4a5b6542473b04e89ff29e47
mrsamia <15203060@iubat.edu>
2021-11-27 21:57:34 +0600

    SRM | input

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App(){
       <h1>Our Medical Services </h1>
       <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ducimus veniam illo quibusdam </h4>
       <Button></Button>
+      <input type="text" placeholder="name"></input>
     </div>
    
   );
