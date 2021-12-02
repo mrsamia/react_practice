0c96b844a7f688a488ea08213bd32d4ca4af750f
mrsamia <15203060@iubat.edu>
2021-12-02 12:03:28 +0600

    SRM | Nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 import Button from './component/Button';
+import Nav from './component/Nav';
+import './component/App_1.css';
+
 
 function App(){
 
   return(
     
-    <div>
+    <div className="content">
+      <Nav></Nav>
       <h1>Our Medical Services </h1>
       <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ducimus veniam illo quibusdam </h4>
-      <Button></Button>
-      <input type="text" placeholder="name"></input>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <input type="text" placeholder="name"></input>
+      </div>
+      
     </div>
    
   );
---
+++ src/component/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Nav.css';
+function Nav(){
+    return(
+    <div class="navbar">
+        <a href="#" class="active">Home</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#" class="right">Link</a>
+    </div>
+    );
+}
+export default Nav;
